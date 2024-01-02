cf8dfed6ae3f6b7d6ecee7e1b64c90212eb90947
s7monk <34889415+s7monk@users.noreply.github.com>
2024-01-02 20:02:13 +0800

    [Improvement] Update package name. (#12)
    * update package name.
    
    * spotless.

---
+++ refutix-server/src/main/java/org/refutix/refutix/server/service/DatabaseService.java
@@ -0,0 +1,62 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.refutix.refutix.server.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import org.refutix.refutix.server.data.dto.DatabaseDTO;
+import org.refutix.refutix.server.data.result.R;
+import org.refutix.refutix.server.data.vo.DatabaseVO;
+
+import java.util.List;
+
+/** Database Service. */
+public interface DatabaseService extends IService<DatabaseVO> {
+
+    /**
+     * Verifies whether the database name is unique.
+     *
+     * @param databaseVO database info
+     * @return result
+     */
+    boolean checkCatalogNameUnique(DatabaseVO databaseVO);
+
+    /**
+     * Creates a new database given {@link DatabaseDTO}.
+     *
+     * @param databaseDTO The {@link DatabaseDTO} object that contains the detail of the created
+     *     database.
+     * @return void indicating the result of the operation.
+     */
+    R<Void> createDatabase(DatabaseDTO databaseDTO);
+
+    /**
+     * Lists databases given catalog id.
+     *
+     * @return The list of databases of given catalog.
+     */
+    R<List<DatabaseVO>> listDatabases(Integer catalogId);
+
+    /**
+     * Drops database given database name.
+     *
+     * @param databaseDTO The dropping database.
+     * @return A response indicating the success or failure of the removal operation.
+     */
+    R<Void> dropDatabase(DatabaseDTO databaseDTO);
+}
---
+++ refutix-server/src/main/java/org/refutix/refutix/server/service/MetadataService.java
@@ -0,0 +1,63 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.refutix.refutix.server.service;
+
+import org.refutix.refutix.server.data.dto.MetadataDTO;
+import org.refutix.refutix.server.data.vo.DataFileVO;
+import org.refutix.refutix.server.data.vo.ManifestsVO;
+import org.refutix.refutix.server.data.vo.SchemaVO;
+import org.refutix.refutix.server.data.vo.SnapshotVO;
+
+import java.util.List;
+
+/** Metadata service includes the service interfaces of metadata. */
+public interface MetadataService {
+
+    /**
+     * Retrieves a list of Metadata schema.
+     *
+     * @param dto query metadata info
+     * @return a list of DatabaseInfo objects
+     */
+    List<SchemaVO> getSchema(MetadataDTO dto);
+
+    /**
+     * Retrieves a list of Metadata snapshot.
+     *
+     * @param dto query metadata info
+     * @return a list of snapshot objects
+     */
+    List<SnapshotVO> getSnapshot(MetadataDTO dto);
+
+    /**
+     * Retrieves a list of Metadata manifest.
+     *
+     * @param dto query metadata info
+     * @return a list of manifest info objects
+     */
+    List<ManifestsVO> getManifest(MetadataDTO dto);
+
+    /**
+     * Retrieves a list of Metadata data file.
+     *
+     * @param dto query metadata info
+     * @return a list of data file objects
+     */
+    List<DataFileVO> getDataFile(MetadataDTO dto);
+}
